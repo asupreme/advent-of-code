51c17b25eacd1b14a9c84539de59edaa11eaff90
asupreme <43858717+asupreme@users.noreply.github.com>
2023-12-06 13:26:24 +0200

    🐒

--- files/2023-06.rb
+++ files/2023-06.rb
@@ -1,7 +1,7 @@
 data = File.read(__FILE__.sub("rb", "txt")).split("\n")
 
 # part 1 — 1624896
-TrueClass.define_method(:to_i){1}; FalseClass.define_method(:to_i){0}
+TrueClass.define_method(:to_i){1}; FalseClass.define_method(:to_i){0} # 🐒
 l1,l2 = *data.map { _1.scan(/\d+/).map(&:to_i) }
 puts(l1.zip(l2).map{|t,d| t.times.map{_1}[1..].reduce(0){|r,h| r+=((t-h)*h > d).to_i}}.compact.reduce(&:*))
 
