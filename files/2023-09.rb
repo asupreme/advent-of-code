56372d0477b7f1a1e5d34890059e50569bc07d5a
asupreme <43858717+asupreme@users.noreply.github.com>
2023-12-09 08:14:32 +0200

    add some whitespace

--- files/2023-09.rb
+++ files/2023-09.rb
@@ -1,21 +1,20 @@
 def step_in(arr)
-    return [0,0] if arr.all?(&:zero?)
-  
-    lvl = arr[1..].map.with_index{_1-arr[_2]}
-    extrapolate(lvl,step_in(lvl))
-  end
-  
-  def extrapolate(lvl,trmnls)
-    [lvl.first - trmnls.first, lvl.last + trmnls.last]
-  end
-  
-  r = File.
-    read(__FILE__.sub("rb","txt")).
-    split("\n").
-    map(&:split).
-    map{_1.map(&:to_i)}.
-    map{extrapolate(_1,step_in(_1))}
-  
-  puts r.map(&:last).sum # 1904165718
-  puts r.map(&:first).sum # 964
-  
+  return [0,0] if arr.all?(&:zero?)
+
+  lvl = arr[1..].map.with_index { _1-arr[_2] }
+  extrapolate(lvl, step_in(lvl))
+end
+
+def extrapolate(lvl,trmnls)
+  [lvl.first - trmnls.first, lvl.last + trmnls.last]
+end
+
+r = File.
+  read(__FILE__.sub("rb", "txt")).
+  split("\n").
+  map(&:split).
+  map { _1.map(&:to_i) }.
+  map { extrapolate(_1, step_in(_1)) }
+
+puts r.map(&:last).sum # 1904165718
+puts r.map(&:first).sum # 964
