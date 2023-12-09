8887300e059e27223eddb441243b1eb34a4b4d4a
asupreme <43858717+asupreme@users.noreply.github.com>
2023-12-09 09:28:53 +0200

    three-lined

--- files/2023-09.rb
+++ files/2023-09.rb
@@ -1,20 +1,4 @@
-def step_in(arr)
-  return [0] if arr.all?(&:zero?)
-
-  lvl = arr[1..].map.with_index { _1-arr[_2] }
-  extrapolate(lvl)
-end
-
-def extrapolate(lvl)
-  trmnls = step_in(lvl)
-  [lvl.first - trmnls.first, lvl.last + trmnls.last]
-end
-
-r = File.
-  read(__FILE__.sub("rb", "txt")).
-  split("\n").
-  map(&:split).
-  map { extrapolate(_1.map(&:to_i)) }
-
-puts r.sum(&:last) # 1904165718
-puts r.sum(&:first) # 964
+def stp(arr); arr.all?(&:zero?) ? [0] : ext(arr[1..].map.with_index { _1-arr[_2] }); end
+def ext(lvl); exts = stp(lvl); [lvl.first-exts.first, lvl.last+exts.last]; end
+puts File.read(__FILE__.sub("rb", "txt")).split("\n").
+  reduce([0,0]) { _1.zip(ext(_2.split.map(&:to_i))).map(&:sum) }.reverse # 1904165718,964
